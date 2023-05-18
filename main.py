3ab6e12f809cee0b17ca375192e44050bb87b2e9
Gilberto Júnior <contato.gil.tulo@gmail.com>
2023-05-18 00:20:23 -0300

    if spam adjustment by match case

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 try:
-    peso = float(input("Digite seu peso em Kg(Exemplo: 60): "))
-    altura = float(input("Digite sua altura em m(Exemplo: 1.70): "))
+    inputPeso = float(input("Digite seu peso em Kg(Exemplo: 60): "))
+    inputAltura = float(input("Digite sua altura em m(Exemplo: 1.70): "))
 
-    imc = peso / (altura * altura)
+    calcularImc = inputPeso / (inputAltura * inputAltura)
 
 except (TypeError, NameError, SyntaxError, ValueError):
     print("================================================")
@@ -17,37 +17,38 @@ except ZeroDivisionError:
     print("================================================")
     input("Pressione qualquer tecla para sair.")
 
-if imc < 18.5:
-    print("================================================")
-    print("Seu IMC é: {0: .2f}.".format(imc))
-    print("Obesidade grau 0, classificação: Magreza.")
-    print("================================================")
-    input("Pressione qualquer tecla para sair.")
+match calcularImc:
+    case calcularImc if calcularImc < 18.5:
+        print("================================================")
+        print("Seu IMC é: {0: .2f}.".format(calcularImc))
+        print("Obesidade grau 0, classificação: Magreza.")
+        print("================================================")
+        input("Pressione qualquer tecla para sair.")
 
-if (imc > 18.5) and (imc < 24.9):
-    print("================================================")
-    print("Seu IMC é: {0: .2f}.".format(imc))
-    print("Obesidade grau 0, classificação: Normal.")
-    print("================================================")
-    input("Pressione qualquer tecla para sair.")
+    case calcularImc if 18.5 < calcularImc < 24.9:
+        print("================================================")
+        print("Seu IMC é: {0: .2f}.".format(calcularImc))
+        print("Obesidade grau 0, classificação: Normal.")
+        print("================================================")
+        input("Pressione qualquer tecla para sair.")
 
-if (imc > 25) and (imc < 29.9):
-    print("================================================")
-    print("Seu IMC é: {0: .2f}.".format(imc))
-    print("Obesidade grau 1, classificação: Sobrepeso.")
-    print("================================================")
-    input("Pressione qualquer tecla para sair.")
+    case calcularImc if 25 < calcularImc < 29.9:
+        print("================================================")
+        print("Seu IMC é: {0: .2f}.".format(calcularImc))
+        print("Obesidade grau 1, classificação: Sobrepeso.")
+        print("================================================")
+        input("Pressione qualquer tecla para sair.")
 
-if (imc > 30) and (imc < 39.9):
-    print("================================================")
-    print("Seu IMC é: {0: .2f}.".format(imc))
-    print("Obesidade grau 2, classificação: Obesidade.")
-    print("================================================")
-    input("Pressione qualquer tecla para sair.")
+    case calcularImc if 30 < calcularImc < 39.9:
+        print("================================================")
+        print("Seu IMC é: {0: .2f}.".format(calcularImc))
+        print("Obesidade grau 2, classificação: Obesidade.")
+        print("================================================")
+        input("Pressione qualquer tecla para sair.")
 
-if imc > 40:
-    print("================================================")
-    print("Seu IMC é: {0: .2f}.".format(imc))
-    print("Obesidade grau 3, classificação: Obesidade Grave.")
-    print("================================================")
-    input("Pressione qualquer tecla para sair.")
+    case calcularImc if calcularImc > 40:
+        print("================================================")
+        print("Seu IMC é: {0: .2f}.".format(calcularImc))
+        print("Obesidade grau 3, classificação: Obesidade Grave.")
+        print("================================================")
+        input("Pressione qualquer tecla para sair.")
